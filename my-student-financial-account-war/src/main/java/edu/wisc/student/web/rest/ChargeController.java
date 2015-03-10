0b8c0f5bae65ae1967e1abec9cf3cea222b5600a
tzodorw <tszodr@gmail.com>
2015-03-10 15:00:48 -0500

    Updated the mapping to include the correct path and id

--- my-student-financial-account-war/src/main/java/edu/wisc/student/web/rest/ChargeController.java
+++ my-student-financial-account-war/src/main/java/edu/wisc/student/web/rest/ChargeController.java
@@ -7,7 +7,9 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,6 +28,7 @@ import edu.wisc.student.finance.v1.ChargeType;
 
 @Api("SPFP API")
 @Controller
+@RequestMapping(value="/api/v1", produces={ MediaType.APPLICATION_JSON_VALUE})
 public class ChargeController {
 
 		@Inject
@@ -38,9 +41,9 @@ public class ChargeController {
 		 */
 		@ApiOperation(value="Get all charges for Student",
 				notes="Retreieve all the charges associated with the student", response=ChargeType.class)
-		@RequestMapping(value="/charges", method=RequestMethod.GET)
-		public Collection<ChargeType> getCharges( String studentIdentifier ){
-			Collection<ChargeType> charges = chargeService.getCharges(studentIdentifier);
+		@RequestMapping(value="/student/{id}/charges", method=RequestMethod.GET)
+		public Collection<ChargeType> getCharges( @PathVariable String id ){
+			Collection<ChargeType> charges = chargeService.getCharges(id);
 			return charges;
 		}
 }
