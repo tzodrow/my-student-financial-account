448108b13e6e1b58d466b3c351ef9656fd120af8
Nicholas Blair <npblair@wisc.edu>
2015-03-16 08:02:11 -0500

    SPFP-48 Implement demo ChargeService
    Add json data example, add implementation of ChargeService. Adds
    Jackson for parsing demo data.
    
    Includes one API breaking change: ChargeType#balanceDue is now "xs:decimal" (coaxed to BigDecimal by JAXB).

---
+++ my-student-financial-account-impl/src/main/java/edu/wisc/student/finance/demo/DemoChargeServiceImpl.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package edu.wisc.student.finance.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.springframework.core.io.ClassPathResource;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.wisc.student.finance.ChargeService;
+import edu.wisc.student.finance.v1.ChargeType;
+
+/**
+ * Demo implementation of {@link ChargeService}.
+ * 
+ * @author Nicholas Blair
+ */
+@Named
+public class DemoChargeServiceImpl implements ChargeService {
+
+  private Map<String, List<ChargeType>> data = Collections.emptyMap();
+  
+  /**
+   * Initialization method called {@link PostConstruct}.
+   * 
+   * @see #readDemoData()
+   * @throws IOException
+   */
+  @PostConstruct
+  protected void init() throws IOException {
+    data = Collections.unmodifiableMap(readDemoData());
+  }
+  /* (non-Javadoc)
+   * @see edu.wisc.student.finance.ChargeService#getCharges(java.lang.String)
+   */
+  @Override
+  public Collection<ChargeType> getCharges(String studentIdentifier) {
+    List<ChargeType> charges = data.get(studentIdentifier);
+    if(charges == null) {
+      return Collections.emptyList();
+    } else {
+      // clone the list so mutations don't go back to persistence
+      return new ArrayList<>(charges);
+    }
+  }
+
+  /**
+   * Reads the demo data and returns it as a {@link Map}.
+   * 
+   * @return
+   * @throws IOException
+   */
+  Map<String,List<ChargeType>> readDemoData() throws IOException {
+    ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
+    
+    try (InputStream is = new ClassPathResource("edu/wisc/student/finance/demo/demo-charge-service-data.json").getInputStream()) {
+      Map<String, List<ChargeType>> result = mapper.readValue(is, new TypeReference<Map<String, List<ChargeType>>>() { });
+      return result;
+    }
+  }
+}
