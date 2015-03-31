c25e95d49ab5f61712601814d1ef9a3ab08f2f0c
Paul Erickson <paul.erickson@wisc.edu>
2015-03-31 17:28:20 +0100

    Update documentation link

--- my-student-financial-account-war/src/main/java/edu/wisc/student/web/SecurityWebApplicationInitializer.java
+++ my-student-financial-account-war/src/main/java/edu/wisc/student/web/SecurityWebApplicationInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 
 /**
  * Spring Security application initializer. This class seems weird, but it's definitely necessary.
- * See <a href="http://docs.spring.io/spring-security/site/docs/3.2.4.RELEASE/reference/htmlsingle/#abstractsecuritywebapplicationinitializer-with-spring-mvc">http://docs.spring.io/spring-security/site/docs/3.2.4.RELEASE/reference/htmlsingle/#abstractsecuritywebapplicationinitializer-with-spring-mvc</a>.
+ * See <a href="http://docs.spring.io/spring-security/site/docs/4.0.0.RELEASE/reference/htmlsingle/#abstractsecuritywebapplicationinitializer-with-spring-mvc">http://docs.spring.io/spring-security/site/docs/4.0.0.RELEASE/reference/htmlsingle/#abstractsecuritywebapplicationinitializer-with-spring-mvc</a>.
  *
  * @author Nicholas Blair
  */
